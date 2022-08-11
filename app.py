0ea9412a72fe45eddadbf8ab6ef0b1e50de00cbd
marhoatumu <52777437+marhoatumu@users.noreply.github.com>
2022-08-11 06:46:20 +0100

    Connected database & added migrations support

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -21,8 +22,10 @@ moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-# TODO: connect to a local postgresql database
+migrate = Migrate(app, db)
 
+# TODO: connect to a local postgresql database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/fyyurApp'
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
